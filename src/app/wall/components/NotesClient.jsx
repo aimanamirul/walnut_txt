a6db23aeb2b9007cb8c0ba1fee082451417ef5d6
aimanamirul <aimanamirul2501@gmail.com>
2023-08-09 21:16:51 +0800

    changing active notes

--- src/app/wall/components/NotesClient.jsx
+++ src/app/wall/components/NotesClient.jsx
@@ -7,13 +7,17 @@ import NoteBox from "./NoteBox";
 
 export default function NotesClient(props) {
     const notes = props.notes;
-    const newNote = {"note_title" : "New Note!", "note_text": "Here is a new note."}
+    const newNote = {"id" : "new", "note_title" : "New Note!", "note_text": "Here is a new note."}
     
     let noteBoxInit = notes ? <NoteBox note={notes[0]} /> : <NoteBox note={newNote} />
     const [noteBoxContent, setNoteBoxContent] = useState(noteBoxInit)
+
+    let activeNoteInit = notes ? notes[0] : newNote;
+    const [activeNoteId, setActiveNoteId] = useState(activeNoteInit.id);
     
     function handleNoteClick(item) {
         setNoteBoxContent(<NoteBox note={item} />)
+        setActiveNoteId(item.id)
     }
 
     return (
@@ -23,7 +27,7 @@ export default function NotesClient(props) {
             </div>
             <div>
                 <ul className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-                    <NotesList notes={notes} handleNoteClick={handleNoteClick} />
+                    <NotesList notes={notes} handleNoteClick={handleNoteClick} activeId={activeNoteId} />
                 </ul>
             </div>
         </main>
--- src/app/wall/components/NotesList.jsx
+++ src/app/wall/components/NotesList.jsx
@@ -1,9 +1,22 @@
 'use client'
 
 export default function NotesList(props) {
+
+    const defineClass = (id) => {
+        let activeId = props.activeId;
+
+        console.log(activeId)
+        console.log(id)
+
+        let itemClass = "mx-2 group rounded-lg border border-gray-400  transition-colors hover:bg-neutral-800/30 px-5 py-4 "
+        let bgColor = (activeId === id) ? "bg-gray-400" : "bg-neutral-800" ;
+        return itemClass + bgColor;
+    }
+
+
     const addNewNote = <li
-        className="mx-2 group rounded-lg border border-gray-400 bg-neutral-800 transition-colors hover:bg-neutral-800/30 px-5 py-4 "
-        onClick={() => { props.handleNoteClick({"note_title": "New Note!", "note_text": ""}) }}
+        className={defineClass("new")}
+        onClick={() => { props.handleNoteClick({"id" : "new" ,"note_title": "New Note!", "note_text": ""}) }}
     >
         <h1 className={`mb-3 text-2xl font-semibold`} >Create A New Note +</h1>
         <p className={`m-0 w-m-[30ch] text-sm opacity-50`}>
@@ -22,7 +35,7 @@ export default function NotesList(props) {
                 {props.notes.map((item, i) => {
                     return (
                         <li
-                            className="mx-2 group rounded-lg border border-gray-400 bg-neutral-800 transition-colors hover:bg-neutral-800/30 px-5 py-4 "
+                            className={defineClass(item.id)}
                             key={item.id}
                             onClick={() => { props.handleNoteClick(item) }}
                         >
