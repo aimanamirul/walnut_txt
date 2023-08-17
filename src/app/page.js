0390504a78c825602c0d3b7c48f82d57ec372b33
Aiman Amirul <aimanamirul2501@gmail.com>
2023-08-17 15:16:25 +0800

    animated bg first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
 
 import NotesClient from "./NotesClient";
 
+import './background.css'
+
 async function getNotes() {
     const res = await fetch('http://127.0.0.1:8090/api/collections/Notes/records', { cache: 'no-store', revalidate: 5 })
     const data = await res.json()
@@ -12,8 +14,8 @@ export default async function NotesWall() {
     const notes = await getNotes();
 
     return (
-        <>
+        <div className="background">
             <NotesClient notes={notes} />
-        </>
+        </div>
     )
 }
