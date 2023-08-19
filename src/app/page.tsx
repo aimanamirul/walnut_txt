a01275d6c5396a8571688ade0781b895381f6f17
aimanamirul <aimanamirul2501@gmail.com>
2023-08-19 10:50:34 +0800

    convert to typescript

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import NotesClient from "./NotesClient";
 import './background.css'
 
 async function getNotes() {
-    const res = await fetch('http://127.0.0.1:8090/api/collections/Notes/records', { cache: 'no-store', revalidate: 5 })
+    const res = await fetch('http://127.0.0.1:8090/api/collections/Notes/records', { cache: 'no-store'})
     const data = await res.json()
     const items = data.items ? data.items : "";
     return items;
