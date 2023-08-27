11c4fc4873f2b69f33e7492daffcf918ce095807
aimanamirul <aimanamirul2501@gmail.com>
2023-08-27 20:40:59 +0800

    some interfaces + date parsing

--- src/app/NotesClient.tsx
+++ src/app/NotesClient.tsx
@@ -1,23 +1,24 @@
 'use client'
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import PocketBase from 'pocketbase'
 
 import NotesList from "./NotesList";
 
 export default function NotesClient(props) {
+    interface Note {
+        id: string;
+        note_title: string;
+        note_text: string;
+        note_date?: string | Date;
+    }
+
     const pb = new PocketBase('http://127.0.0.1:8090');
     const router = useRouter()
     const notes = props.notes;
-    const newNote = { "id": "new", "note_title": "New Note!", "note_text": "Here is a new note." }
-
-    // let noteBoxInit = notes ? <NoteBox note={notes[0]} saveNote={saveNote} /> : <NoteBox note={newNote} saveNote={saveNote} />
-    // const [noteBoxContent, setNoteBoxContent] = useState(noteBoxInit)
-
-    // let activeNoteInit = notes ? notes[0] : newNote;
-
-    let initNote = notes ? notes[0] : newNote;
+    const newNote = { "id": "new", "note_title": "", "note_text": "" } as Note;
+    let initNote = newNote;
     const [activeNoteId, setActiveNoteId] = useState(initNote.id);
     const [title, setTitle] = useState(initNote.note_title);
     const [text, setText] = useState(initNote.note_text)
@@ -28,15 +29,15 @@ export default function NotesClient(props) {
         setActiveNoteId(item.id);
     }
 
-    const createNote = async(e) => {
+    const createNote = async (e) => {
         e.preventDefault()
         let data = {
             "note_title": title,
             "note_text": text,
-            "note_date": ""
+            "note_date": new Date(),
         }
 
-        if(activeNoteId === "new") {
+        if (activeNoteId === "new") {
             const record = await pb.collection('Notes').create(data);
         } else {
             const record = await pb.collection('Notes').update(activeNoteId, data)
@@ -44,11 +45,18 @@ export default function NotesClient(props) {
 
         setText('');
         setTitle('');
+        setActiveNoteId('new');
         router.refresh();
     }
 
-    const removeNote = async(e) => {
+    const removeNote = async (e, noteId) => {
+        // console.log(noteId);
+        await pb.collection('Notes').delete(noteId);
 
+        setText('');
+        setTitle('');
+        setActiveNoteId('new');
+        router.refresh();
     }
 
     return (
@@ -56,20 +64,25 @@ export default function NotesClient(props) {
             <div>
                 <form className="flex justify-between flex-col items-center w-[85vw]" onSubmit={createNote}>
                     <div className="bg-slate-900 w-full h-[70vh] m-5 p-5 rounded-lg border border-gray-400">
-                        <div className="flex flex-row">
-                            <input
-                                id="note_title"
-                                name="note_title"
-                                onChange={(e) => { setTitle(e.target.value) }}
-                                className="flex-1 py-1 text-xl font-extrabold bg-slate-900 outline-none resize-none" type="text" value={title} />
-                            <button className="flex-3 min-w-[10%] mx-1 text-white font-extrabold outline-none border-none rounded-2xl bg-cyan-500" type="submit">Save</button>
-                            <button className="flex-3 min-w-[10%] mx-1 text-black font-extrabold outline-none border-none rounded-2xl bg-white" onClick={(e) => {
-                                e.preventDefault();
-                            }}>Clear Note</button>
+                        <div className="w-full flex flex-col lg:flex-row">
+                            <div className="lg:flex-1">
+                                <input
+                                    id="note_title"
+                                    name="note_title"
+                                    placeholder="Write your Title here!"
+                                    onChange={(e) => { setTitle(e.target.value) }}
+                                    className="w-full py-1 text-xl font-extrabold bg-slate-900 outline-none resize-none" type="text" value={title} />
+                            </div>
+                            <div className="flex flex-row">
+                                <button className="min-w-[10%] mx-1 px-3 py-2 text-white font-extrabold outline-none border-none rounded-2xl bg-cyan-500" type="submit">Save</button>
+                                <button className="min-w-[10%] mx-1 px-3 py-2 text-black font-extrabold outline-none border-none rounded-2xl bg-white" onClick={(e) => {
+                                    e.preventDefault();
+                                }}>Clear Note</button>
+                            </div>
                         </div>
                         <hr className="my-3" />
                         <textarea
-                            className="w-full font-['Consolas'] min-h-[87%] bg-slate-900 outline-none resize-none"
+                            className="w-full font-['Consolas'] min-h-[80%] lg:min-h-[87%] bg-slate-900 outline-none resize-none"
                             value={text}
                             placeholder="Write your note here!"
                             id="note_text"
@@ -82,7 +95,7 @@ export default function NotesClient(props) {
             </div>
             <div>
                 <ul className="mb-16 grid text-center lg:mb-0 lg:grid-cols-3 lg:text-left">
-                    <NotesList notes={notes} handleNoteClick={handleNoteClick} activeId={activeNoteId} />
+                    <NotesList notes={notes} handleNoteClick={handleNoteClick} removeNote={removeNote} activeId={activeNoteId} />
                 </ul>
             </div>
         </main>
--- src/app/NotesList.tsx
+++ src/app/NotesList.tsx
@@ -4,6 +4,31 @@ import { useState } from "react";
 import { useRouter } from "next/navigation";
 
 export default function NotesList(props) {
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    interface parsedDate {
+        dayString: string;
+        dayNumber: number;
+        month: string;
+        year: string | number;
+    }
+
+    function parseDate<Date>(noteDate): parsedDate {
+        let parsingDate = new Date(noteDate);
+        let day = days[parsingDate.getDay()];
+        let month = months[parsingDate.getMonth()];
+        let year = parsingDate.getFullYear();
+
+        let parsedDate = {
+            dayString: day,
+            dayNumber: parsingDate.getUTCDate(),
+            month: month,
+            year: year,
+        };
+
+        return parsedDate as parsedDate;
+    }
 
     //const [localNotes, setlocalNotes] = useState(props.notes)
     const localNotes = props.notes
@@ -12,13 +37,13 @@ export default function NotesList(props) {
     const defineClass = (id = "new") => {
         let activeId = props.activeId;
         let itemClass = "my-2 mx-2 group rounded-lg border border-gray-400  transition-colors hover:bg-neutral-800/30 px-4 py-3 "
-        let bgColor = (activeId === id) ? "bg-gray-400" : "bg-neutral-800" ;
+        let bgColor = (activeId === id) ? "bg-gray-400" : "bg-neutral-800";
         return itemClass + bgColor;
     }
 
     const newNoteSticky = <li
         className={defineClass()}
-        onClick={() => { props.handleNoteClick({"id" : "new" ,"note_title": "New Note!", "note_text": ""}) }}
+        onClick={() => { props.handleNoteClick({ "id": "new", "note_title": "New Note!", "note_text": "" }) }}
     >
         <h1 className={`mb-3 text-2xl font-semibold`} >Create A New Note +</h1>
         <p className={`m-0 w-m-[30ch] text-sm opacity-50`}>
@@ -32,19 +57,26 @@ export default function NotesList(props) {
             {newNoteSticky}
         </>)
     } else {
+
         return (
             <>
                 {localNotes.map((item, i) => {
+                    let itemParsedDate : parsedDate = parseDate(item.note_date);
+
                     return (
                         <li
                             className={defineClass(item.id)}
                             key={item.id}
                             onClick={() => { props.handleNoteClick(item) }}
                         >
-                            <h1 className={`mb-3 text-2xl font-semibold`} >{item.note_title}</h1>
-                            <p className={`m-0 w-m-[30ch] text-sm opacity-50`}>
+                            <h1 className={`mb-1 break-words text-2xl font-semibold`} >{item.note_title}</h1>
+                            <p className={`mb-1 w-m-[25ch] text-sm opacity-50`}>
+                                {`${itemParsedDate.dayString} - ${itemParsedDate.dayNumber} ${itemParsedDate.month} ${itemParsedDate.year}`}
+                            </p>
+                            <p className={`mb-1 w-m-[25ch] text-sm opacity-50`}>
                                 {item.note_text}
                             </p>
+                            <button onClick={e => { e.stopPropagation(); if (confirm('Delete this note?')) props.removeNote(e, item.id); }} className="my-1 mt-2 bg-red-800 border rounded-md px-2 hover:bg-red-500">Delete </button>
                         </li>
                     )
                 })
